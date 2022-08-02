54895425f5352fd6663c64fe3251431eda2feead
Ericksosa <106286155+Ericksosa@users.noreply.github.com>
2022-08-01 21:47:37 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Alice</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="icon" href="images/logo_icono.png">
+</head>
+<div id="contenedor">
+<body>
+</div>
+    <header>
+        <div class="container">
+            
+        <img class="imagen" src="images/logo_icono.png" alt="Logo">
+        
+            
+            <nav>
+                <a href="#somos-proya">Ayuda</a>
+                <a href="Descargas/Alice.exe" download="Alice.exe">Descargar</a>
+                <a href="#caracteristicas">Contactar</a>
+                <a href="signup.php">Registrate!</a>
+            </nav>
+        </div>
+    </header>
+
+    <div id="hero">
+        
+        <h1>Alice <br>Sistema contable</h1>
+        <a href="Descargas/Alice.exe" download="Alice.exe">
+        <button>Descargar</button>
+        </a>
+</div>
+</body>
+</html>
